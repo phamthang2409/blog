51bdd99198f4b523f700aaa9cc715692c93cf700
phamthang2409 <149677255+phamthang2409@users.noreply.github.com>
2024-10-07 13:58:35 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+const handlebars = require('express-handlebars').engine;
+const path = require('path');
+
+const route = require('./routes')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+//template engine
+app.engine('hbs', handlebars({
+extname: ".hbs"
+}))
+app.set('view engine','hbs')
+app.set("views", path.join(__dirname, "resources\\views"));
+
+route(app);
+
+//http logger
+app.use(morgan('combined'))
+
+    
+app.listen(port, ()=> console.log("Server is running on port: ", port));
